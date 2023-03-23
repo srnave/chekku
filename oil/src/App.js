25bf5de414c2ba8218a8d16f836a41dc59e5e1e7
NAVEEN S R <srnave12@gmail.com>
2023-03-23 15:16:41 +0530

    footer

--- oil/src/App.js
+++ oil/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 //import Login from './Login'
 import Form from './Form';
+import Footer from './Footer';
 function App() {
   return (
     <div className="App">
     {/* <Login/>  */}
-    <Form/>
+    <Form/>  
+    <Footer/> 
+    
     </div>
   );
 }
---
+++ oil/src/Footer.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <>
+    <footer>
+
+<div class="row">
+
+  <div class="column">
+
+    <h4>About Us</h4>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in metus euismod, faucibus metus ut, semper nibh. Aenean euismod justo eu enim dapibus suscipit.</p>
+
+  </div>
+
+  <div class="column">
+
+    <h4>Quick Links</h4>
+
+    <ul>
+
+      <li><a href="#"><i class="fa fa-angle-right"></i> Subscription</a></li>
+
+      <li><a href="#"><i class="fa fa-angle-right"></i> Contact us</a></li>
+
+      <li><a href="#"><i class="fa fa-angle-right"></i> Bug report</a></li>
+
+    </ul>
+
+  </div>
+
+  <div class="column">
+
+    <h4>Connect with Us</h4>
+
+    <ul class="social-icons">
+
+      <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+
+      <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+      
+      <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+
+        <li><a href="#"><i class="fa-brands fa-github"></i></a></li>
+
+    </ul>
+
+  </div>
+
+</div>
+
+<p class="copyright">© 2023 All Rights Reserved</p>
+
+</footer>
+</>
+  )
+}
