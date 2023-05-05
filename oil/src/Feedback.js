c4bfd801f15d0024b8f1dc122f2f7cb28977a629
Meianbu <meianbu2723@gmail.com>
2023-05-05 20:18:43 +0530

    made to sent email in feedback form

--- oil/src/Feedback.js
+++ oil/src/Feedback.js
@@ -1,23 +1,36 @@
 import React from "react";
 import './Feedback.css';
 const Feedback=()=>{
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_5qrvfn7', 'template_v9jdpfi', form.current, 'uxdKIFc-iLYILYTWU')
+      .then((result) => {
+          console.log(result.text);
+          console.log("Sent");
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 return(
     <>   
     <center> 
     <div class="container">
   
     <label for="fname">Name</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+    <input type="text" id="fname" name="name" placeholder="Your name.."/>
 
     <label for="lname">Email</label>
-    <input type="email" id="lname" name="lastname" placeholder="Your email.."/>
+    <input type="email" id="user_email" name="user_email" placeholder="Your email.."/>
 
    
 
     <label for="subject">Subject</label>
-    <textarea id="subject" name="subject" placeholder="Your feedback.." ></textarea>
+    <textarea id="message" name="message" placeholder="Your feedback.." ></textarea>
 
-    <input type="submit" value="Submit"/>
+    <input   type="submit"  class="button .button1" value="SEND" ></input>
 
 </div>
 
